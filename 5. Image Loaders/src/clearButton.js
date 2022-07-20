060c8ae6e00bee244e99d324f45ab20bf758c071
Ram kumar <webbieram@outlook.com>
2022-07-20 15:42:33 +0530

    handling images and image loader

--- 5. Image Loaders/src/clearButton.js
+++
@@ -1,9 +0,0 @@
-import style from './clearButton.scss';
-
-const el = document.createElement('button');
-el.innerHTML = 'Clear';
-el.classList.add([style.button]);
-el.onclick = function() {
-    console.log('Clear button clicked');
-}
-document.body.appendChild(el);
